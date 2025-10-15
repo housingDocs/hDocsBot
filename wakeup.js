bd8716713b77d445807ade24dd273cf30bb9a997
cookieCrood <cookie.crood@gmail.com>
2025-10-15 16:07:48 +0200

    followup

--- wakeup.js
+++ wakeup.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
-const URL = ""
+const URL = "https://hdocsbot.onrender.com"
 
 app.get("/wakeup", (req, res) => {
     
